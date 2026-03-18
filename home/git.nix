5a88ee23c986e155f3c8caccf00f63ef2e72b91a
KiRura <57588049+KiRura@users.noreply.github.com>
2026-03-19 04:26:29 +0900

    gitの追加, コメントの削除, fonts.nixに分離

---
+++ home/git.nix
@@ -0,0 +1,12 @@
+{pkgs, ...}:
+{
+   programs.git = {
+      enable = true;
+      settings = {
+         user = {
+           name = "KiRura";
+            email = "57588049+KiRura@users.noreply.github.com";
+         };
+      };
+   };
+}
